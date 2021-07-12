87bfa03420d8b16a45905c94f50e6c6926e1ac66
GabrieleRochaB <gabriele.rocha@apisys.com.br>
2021-07-12 11:55:53 -0300

    Modulo 14 - Exercicio 01

--- Modulo 14/lista/v1.0/db/schema.rb
+++ Modulo 14/lista/v1.0/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_131557) do
+ActiveRecord::Schema.define(version: 2021_07_12_135246) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
